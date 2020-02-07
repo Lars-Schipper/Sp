8a0d40d7671de23636061d106a157e8fb507088b
Lars-Schipper <60731698+Lars-Schipper@users.noreply.github.com>
2020-02-07 13:20:37 +0100

    opdracht 9 afgemaakt alleen het 2e deel schuift nog de verkeerde kan op

--- formatieve opdracht 1/opdracht_9.py
+++ formatieve opdracht 1/opdracht_9.py
@@ -1,15 +1,20 @@
 def schuifer(ch, n):
 
     if n < 0:
+        lines = ch
+        for i in range(n, 0):
+            lines = lines[-1:]+ lines[:-1]
+        return lines
 
-
-        print(n)
     elif n > 0:
-        print(ch << n)
+        lines = ch
+        for i in range(0, n+1):
+            lines = lines[-1:]+ lines[:-1]
+        return lines
     elif n == 0:
-        print('er word vandaag niet geschoven want n = 0')
+        return 'er word vandaag niet geschoven want n = 0'
 
-ch = 1011000
-n = 3
-
-schuifer(ch, n)
+ch = '1011000'
+n = int(input('met welke factor wilt u verschuiven?: '))
+print('begin waarde ch =', ch)
+print(schuifer(ch, n))

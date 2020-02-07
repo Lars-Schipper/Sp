ddcbe2bb24ce5565d11a4d678ab016f5892c9450
Lars-Schipper <60731698+Lars-Schipper@users.noreply.github.com>
2020-02-07 12:25:14 +0100

    alle files hernoemd

--- formatieve opdracht 1/opdracht_8.py
+++ formatieve opdracht 1/opdracht_8.py
@@ -1,9 +1,9 @@
-infile = open('opdracht 8.txt', 'r')
+infile = open('opdracht_8.txt', 'r')
 lines = infile.readlines()
 infile.close()
 
 lines = [line.replace(' ','')for line in lines]
 lines = [line.replace('\n','')for line in lines]
 
-outfile = open('opdracht 8.txt', 'w')
+outfile = open('opdracht_8.txt', 'w')
 outfile.writelines(lines)

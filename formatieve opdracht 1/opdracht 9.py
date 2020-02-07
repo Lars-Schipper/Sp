87c73ab7c084d96dbba0f43fc69b1eb3d2c4a4d6
Lars-Schipper <60731698+Lars-Schipper@users.noreply.github.com>
2020-02-07 12:21:05 +0100

    opdracht 7 & 8 toegevoegd

---
+++ formatieve opdracht 1/opdracht 9.py
@@ -0,0 +1,15 @@
+def schuifer(ch, n):
+
+    if n < 0:
+
+
+        print(n)
+    elif n > 0:
+        print(ch << n)
+    elif n == 0:
+        print('er word vandaag niet geschoven want n = 0')
+
+ch = 1011000
+n = 3
+
+schuifer(ch, n)

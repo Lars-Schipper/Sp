0e8e8992c02b9692ef4d45396de0fcfc1c45bcdd
Lars-Schipper <60731698+Lars-Schipper@users.noreply.github.com>
2020-02-14 13:14:59 +0100

    begin gemaakt met mastermind

--- 1. mastering mastermind/mastermind.py
+++ 1. mastering mastermind/mastermind.py
@@ -1,66 +1,29 @@
 import random
+import funties
 
-
-def check(secret, gok):
-    print('secret = ', secret, 'gok = ', gok)
-
-    if secret == gok:
-        print('gefeliciteerd!!')
-        return True
-
-    elif secret != gok:
-
-
-        return False
-
-def input_secret():
-    secret = input('voer je code in: ').split(',')
-
-    while len(secret) != 4:
-        secret = input('voer een geldige code van 4 cijfers in: ').split(',')
-
-    return secret
-
-def input_gok():
-    gok = input('voer je gok in: ').split(',')
-
-    while len(gok) != 4:
-        gok = input('voer een geldige gok van 4 cijfers in: ').split(',')
-
-    return gok
-
-def gok_generator():
-    gok = []
-    for i in range(0, 4):
-        gok.append(kleur_opties[random.randrange(0, 6)])
-
-    return gok
-
-def secret_generator():
-    secret_g = []
-    for i in range(0, 4):
-        secret_g.append(kleur_opties[random.randrange(0, 6)])
-
-    return secret_g
 
 
 kleur_opties = ['1', '2', '3', '4', '5', '6']   #kleuren opties definiëren
 
-# main_loop
+# main_loop ===============================================
 while True:
     wie_raad = input('wil je dat de computer raad?(y/n/q)')
 
     if wie_raad == 'y': # de computer raad
         a = input_secret()
+        b = gok_generator()
         i = 0
         while True:
             i += 1
             print(i)
 
-            if check(a, gok_generator()) == True:
+            # if check(a, gok_generator()) == True:  #de computer zelf automatisch laten raden
+            #     break
+
+            if check(a, b) == True:
                 break
     # ======================================================
-    elif wie_raad == 'n': # ik raad
+    elif wie_raad == 'n':               # ik raad
 
         secret = secret_generator()
         print(secret)
@@ -68,6 +31,6 @@ while True:
             if check(secret, input_gok()) == True:
                 break
     #========================================================
-    elif wie_raad == 'q': #als q wordt ingevoerd eindigt het programma
+    elif wie_raad == 'q':               # als q wordt ingevoerd eindigt het programma
         print('bedankt voor het spelen')
         break

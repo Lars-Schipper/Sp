f716e5f470b1165c0c0365bff5d5064f73bc2e6f
Lars-Schipper <60731698+Lars-Schipper@users.noreply.github.com>
2020-02-17 14:52:10 +0100

    begin gemaakt met mastermind

---
+++ 1. mastering mastermind/lijst test.py
@@ -0,0 +1,8 @@
+lijst = []
+for i in range(1, 7):
+    for j in range(1, 7):
+        for a in range(1, 7):
+            for b in range(1,7):
+                lijst.append([i,j,a,b])
+
+print(lijst)

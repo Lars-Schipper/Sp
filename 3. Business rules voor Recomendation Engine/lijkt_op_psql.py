4334a923b99f44452093747cc8ca0bd08f6fb546
Lars-Schipper <60731698+Lars-Schipper@users.noreply.github.com>
2020-03-20 19:25:28 +0100

    kan nu 3 producten recomenden bij een product

--- 3. Business rules voor Recomendation Engine/lijkt_op_psql.py
+++ 3. Business rules voor Recomendation Engine/lijkt_op_psql.py
@@ -9,23 +9,27 @@ with open('lijkt_op.csv') as file:
     counter = 0
     lijst = []
     lijst_tijdelijk = []
-    lijst_voor_vergelijking = []
+    lijst_vergelijkbaren_producten = []
     for line in csv_reader:
         if counter != 0:
             lijst.append(line)
         counter += 1
-    print(lijst)
+
     print(len(lijst))
 
     i = 0
     for element in lijst:
-        for i in range(0, len(lijst)):
-            print(i)
-            # if len(lijst_tijdelijk) <= 3:
+        print(element, i)
+        lijst_vergelijkbaren_producten.append(element)
+        for teller in range(0, len(lijst)):
 
-                # if element[1] == i[1] and element[2] == i[2]:
-                #     lijst_voor_vergelijking.append(i)
-    print(lijst_voor_vergelijking)
+            if lijst[teller][1] == element[1] and lijst[teller][2] == element[2]:
+                if len(lijst_vergelijkbaren_producten) < 4 and lijst[teller] != element:
+                    lijst_vergelijkbaren_producten.append(lijst[teller])
+        print(lijst_vergelijkbaren_producten)
+        break
+
+        i+=1
 
 cur.close()
 c.close()
